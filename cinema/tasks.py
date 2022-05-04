36fada5d8442108ae9c79bbf688b0cd8c5507e3b
Bagdad02 <bagdadzhunushov@gmail.com>
2022-05-04 17:02:01 +0600

    added online cinema

---
+++ cinema/tasks.py
@@ -0,0 +1,19 @@
+import time
+
+from celery import shared_task
+from django.core.mail import send_mail
+
+from cinema.celery import app
+
+
+@app.task
+def send_confirmation_email(code,email):
+    time.sleep(10)
+    full_link = f'http://localhost:8000/api/v1/account/activate/{code}'
+
+    send_mail(
+        'привет',
+        full_link,
+        'bagdadzhunushov@gmail.com',
+        [email],
+    )
